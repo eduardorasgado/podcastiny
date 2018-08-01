3c5f5ed753d8c56c354822bf69291410bfc5411f
lal <eduardo.rasgado@hotmail.com>
2018-08-01 12:50:13 -0500

    styling to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 export default class extends React.Component {
 	render() {
-		return <h1>Hola mundo!</h1>
+		return (
+				<div>
+					<h1>Hola mundo!</h1>
+					<p>Bienvenidos al curso de nextJS</p>
+
+					<style jsx>{`
+							h1 {
+								color: #0791E6;
+							}
+							p {
+								color: #7FD136;
+							}
+						`}</style>
+
+						<style jsx global>{`
+							body {
+								background: #252620;
+							}
+						`}</style>
+				</div>
+			)
 	}
 }
