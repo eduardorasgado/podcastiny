0950e709df98a8ee0c30606d487864822df677a9
lal <eduardo.rasgado@hotmail.com>
2018-08-01 14:26:07 -0500

    visualize the channels in index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
+// gracias a esta libreria funciona fetch
+import 'isomorphic-fetch'
+
 export default class extends React.Component {
+
+	//llama a la api, recibe los canales y los inserta a las props
+	static async getInitialProps() {
+		let req = await fetch('https://api.audioboom.com/channels/recommended')
+		let { body: channels } = await req.json()
+
+		return { channels }
+	}
 	render() {
+		//const channels = this.props.channels
+		const { channels } = this.props
 		return (
 				<div>
-					<header>Podcastiny App</header>
+					<header>Podcastiny</header>
+
+					{ channels.map((channel) => (
+						<div className="channel">
+							<img src={ channel.urls.logo_image.original} alt="" />
+							<h2>{ channel.title }</h2>
+						</div>
+						)) }
 
 					<style jsx global>{`
 						body {
@@ -14,9 +34,29 @@ export default class extends React.Component {
 					`}</style>
 
 					<style jsx>{`
-						color: #fff;
-						background: #8756ca;
-						padding: 15px;
+						header {
+							color: #fff;
+							background: #8756ca;
+							padding: 15px;
+						}
+						.channel {
+							display: block;
+							border-radius: 3px;
+							box-shadow: 0px 2px 6px rgba(0,0,0, 0.15);
+							margin-bottom: 0.5em;
+						}
+
+						.channel img {
+							width: 100%;
+						}
+
+						h2 {
+							padding: 5px;
+							font-size: 0.9em;
+							font-weight: 600;
+							margin: 0;
+							text-align: center;
+						}
 					`}</style>
 				</div>
 			)
