d2511990efeb667b3244a56d2387c5c4f92aa5b6
lal <eduardo.rasgado@hotmail.com>
2018-08-01 13:05:35 -0500

    img added and styling

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,37 @@ export default class extends React.Component {
 	render() {
 		return (
 				<div>
+					<img src="/static/platzi-logo.png" alt="platzi logo" />
 					<h1>Hola mundo!</h1>
 					<p>Bienvenidos al curso de nextJS</p>
+
+					
 
 					<style jsx>{`
 							h1 {
 								color: #0791E6;
+								font-family: "Lato";
+								width: 200px;
+								margin: auto;
+								margin-top: 40px;
 							}
-							p {
+
+							/*afecta a todos los p de nuestra aplicacion*/
+							/*Si fuera (div p) -> todos los p dentro de un div tendran el style*/
+
+							:global(p) {
 								color: #7FD136;
+								font-family: "Lato";
+								width: 240px;
+								margin: auto;
+								margin-bottom: 40px;
+							}
+
+							img {
+								max-width: 50%;
+								display: block;
+								margin 0 auto;
+								margin-top: 40px;
 							}
 						`}</style>
 
