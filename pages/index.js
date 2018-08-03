f898459de3d6756db35ea08da567bd4a4045462c
lal <eduardo.rasgado@hotmail.com>
2018-08-03 13:42:09 -0500

    api request erro handling in index

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,25 @@ export default class extends React.Component {
 
 	//llama a la api, recibe los canales y los inserta a las props
 	static async getInitialProps() {
-		let req = await fetch('https://api.audioboom.com/channels/recommended')
-		let { body: channels } = await req.json()
+		try {
+			let req = await fetch('https://api.audioboom.com/channels/recommended')
+			let { body: channels } = await req.json()
 
-		// retornar como parte de las props
-		return { channels }
+			// retornar como parte de las props
+			return { channels, statusCode: 200 }
+		} catch(e) {
+			return { channels: null, statusCode: 503 }
+		}
 	}
 	render() {
 		//const channels = this.props.channels
-		const { channels } = this.props
+		const { channels, statusCode } = this.props
+		// early return
+		if(statusCode !== 200) {
+			return <Layout title="Podcastiny: ERROR">
+				<div>Ops! Parece que algo rompió el server o la conección</div>
+			</Layout>
+		}
 		return (
 				<Layout title="Podcastiny">
 
