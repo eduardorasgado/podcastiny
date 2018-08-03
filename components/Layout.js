9a9d49e7667cf94b4c9b747c9da17237ad610df1
lal <eduardo.rasgado@hotmail.com>
2018-08-02 22:58:17 -0500

    head to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default class Layout extends React.Component {
 	render() {
-		const { children } = this.props
+		const { children, title } = this.props
 		return (
 				<div>
+				<Head>
+					<title>{title}</title>
+				</Head>
 				<header><Link href="/"><a>Podcastiny</a></Link></header>
 				{ children }
 				<style jsx global>{`
