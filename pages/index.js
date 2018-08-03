0e516449fe807867e1f4f31345d466b7391ef006
lal <eduardo.rasgado@hotmail.com>
2018-08-03 14:24:55 -0500

    error code in index

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import 'isomorphic-fetch'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import ChannelGrid from '../components/ChannelGrid'
+import Error from 'next/error'
 
 export default class extends React.Component {
 
 	//llama a la api, recibe los canales y los inserta a las props
-	static async getInitialProps() {
+	static async getInitialProps({ res }) {
 		try {
 			let req = await fetch('https://api.audioboom.com/channels/recommended')
 			let { body: channels } = await req.json()
@@ -16,6 +17,7 @@ export default class extends React.Component {
 			// retornar como parte de las props
 			return { channels, statusCode: 200 }
 		} catch(e) {
+			res.statusCode = 503
 			return { channels: null, statusCode: 503 }
 		}
 	}
@@ -24,9 +26,7 @@ export default class extends React.Component {
 		const { channels, statusCode } = this.props
 		// early return
 		if(statusCode !== 200) {
-			return <Layout title="Podcastiny: ERROR">
-				<div>Ops! Parece que algo rompió el server o la conección</div>
-			</Layout>
+			return <Error statusCode={ statusCode } />
 		}
 		return (
 				<Layout title="Podcastiny">
