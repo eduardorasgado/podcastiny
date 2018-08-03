43707180a939cde8e73073edc42e220b43870674
lal <eduardo.rasgado@hotmail.com>
2018-08-02 23:15:10 -0500

    home refactor concluded: grid channels in its own component

---
+++ components/ChannelGrid.js
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+
+export default class ChannelGrid extends React.Component {
+	render () {
+		const { children, channels } = this.props
+		return (
+				<div className="channels">
+					{ channels.map((channel) => (
+						// link permite prerenderizar del lado del cliente
+						// cuando hay que cargar algo liviano, prefetch ayuda a no cargar el getInitialProos
+						// prefetch solo carga html, css, js
+						// prefetch solo funciona en produccion
+						// npm run build && npm start
+						<Link key={channel.id} href={`/channel?id=${ channel.id }`} prefetch>
+							<div className="channel">
+								<img src={ channel.urls.logo_image.original} alt="" />
+								<h2>{ channel.title }</h2>
+							</div>
+						</Link>
+					)) }
+
+					<style jsx>{`
+					/* CSS GRID */
+					.channels {
+						display: grid;
+						grid-gap: 15px;
+						padding: 15px;
+						grid-template-columns: repeat(auto-fill, minmax(120px, 1fr))
+					}
+
+					.channel {
+						display: block;
+						border-radius: 3px;
+						box-shadow: 0px 2px 6px rgba(0,0,0, 0.15);
+						margin-bottom: 0.5em;
+					}
+
+					.channel img {
+						width: 100%;
+					}
+
+					h2 {
+						padding: 5px;
+						font-size: 0.9em;
+						font-weight: 600;
+						margin: 0;
+						text-align: center;
+					}
+				`}</style>
+			</div>
+			)
+	}
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import 'isomorphic-fetch'
 //libreria para linkear componentes
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import ChannelGrid from '../components/ChannelGrid'
 
 export default class extends React.Component {
 
@@ -19,56 +20,9 @@ export default class extends React.Component {
 		const { channels } = this.props
 		return (
 				<Layout title="Podcastiny">
-					<div className="channels">
-						{ channels.map((channel) => (
-							// link permite prerenderizar del lado del cliente
-							// cuando hay que cargar algo liviano, prefetch ayuda a no cargar el getInitialProos
-							// prefetch solo carga html, css, js
-							// prefetch solo funciona en produccion
-							// npm run build && npm start
-							<Link key={channel.id} href={`/channel?id=${ channel.id }`} prefetch>
-								<div className="channel">
-									<img src={ channel.urls.logo_image.original} alt="" />
-									<h2>{ channel.title }</h2>
-								</div>
-							</Link>
-						)) }
-					</div>
 
-					<style jsx>{`
-						header {
-							color: #fff;
-							background: #8756ca;
-							padding: 15px;
-						}
-
-						/* CSS GRID */
-						.channels {
-							display: grid;
-							grid-gap: 15px;
-							padding: 15px;
-							grid-template-columns: repeat(auto-fill, minmax(120px, 1fr))
-						}
-
-						.channel {
-							display: block;
-							border-radius: 3px;
-							box-shadow: 0px 2px 6px rgba(0,0,0, 0.15);
-							margin-bottom: 0.5em;
-						}
-
-						.channel img {
-							width: 100%;
-						}
-
-						h2 {
-							padding: 5px;
-							font-size: 0.9em;
-							font-weight: 600;
-							margin: 0;
-							text-align: center;
-						}
-					`}</style>
+					<ChannelGrid channels={ channels } />
+					
 				</Layout>
 			)
 	}
