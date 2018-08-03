786ffee2f12c8c31ccb3df4dc630b7bbc46f0971
lal <eduardo.rasgado@hotmail.com>
2018-08-02 21:31:41 -0500

    podcast page for mp3 player

--- pages/channel.js
+++ pages/channel.js
@@ -1,3 +1,6 @@
+import 'isomorphic-fetch'
+import Link from 'next/link'
+
 export default class extends React.Component {
 
 	// clonsumiendo API
@@ -14,13 +17,15 @@ export default class extends React.Component {
 		])
 
 		// ahora a construir las json para ocuparlas en el component
-		let dataChannel = await reqChannel.json()
+		let [dataChannel, dataAudios, dataSeries] = await Promise.all([
+			reqChannel.json(),
+			reqAudios.json(),
+			reqSeries.json()
+		])
+
+		// extraemos los features que queremos de los json
 		let channel = dataChannel.body.channel
-
-		let dataAudios = await reqAudios.json()
 		let audioClips = dataAudios.body.audio_clips
-
-		let dataSeries = await reqSeries.json()
 		let series = dataSeries.body.channels
 
 		// retornar como parte de las props
@@ -33,62 +38,119 @@ export default class extends React.Component {
 
 		return(
 				<div>
-				<header>Podcastiny</header>
+					<header>Podcastiny</header>
+
+	      	<div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
+					
+					<nav>
+            <Link href={`/`}>
+              <a className='close'>&lt; Inicio</a>
+            </Link>
+          </nav>
 					<h1>{channel.title}</h1>
 
-					<h2>Series</h2>
-					{ series.map((serie) => (
+					{
+						series.length > 0 &&
 						<div>
-							{ serie.title }
+							<h2>Series</h2>
+							{ series.map((serie) => (
+								<div className="channels">
+									<Link href={`/channel?id=${serie.id}`}>
+										<a className="channel">
+											<img src={serie.urls.logo_image.original} />
+											<h2>{ serie.title }</h2>
+										</a>
+									</Link>
+								</div>
+							))}
 						</div>
-					))}
+					}
+
 					<h2>Últimos Podcast</h2>
 					{ audioClips.map((clip) => (
-						<div key={clip.id}>
-							{ clip.title }
-						</div>
+						<Link key={clip.id} href={`/podcast?id=${clip.id}`}>
+							<a className="podcast">
+								<h3>{ clip.title }</h3>
+								<div className="meta">
+									{Math.ceil(clip.duration / 60)} minutos
+								</div>
+							</a>
+						</Link>
 					))}
 
 					<style jsx>{`
-						header {
-							color: #fff;
-							background: #8756ca;
-							padding: 15px;
-						}
+		        header {
+		          color: #fff;
+		          background: #8756ca;
+		          padding: 15px;
+		          text-align: center;
+		        }
 
-						/* CSS GRID */
-						.channels {
-							display: grid;
-							grid-gap: 15px;
-							padding: 15px;
-							grid-template-columns: repeat(auto-fill, minmax(120px, 1fr))
-						}
+		        .banner {
+		          width: 100%;
+		          padding-bottom: 25%;
+		          background-position: 50% 50%;
+		          background-size: cover;
+		          background-color: #aaa;
+		        }
 
-						.channel {
-							display: block;
-							border-radius: 3px;
-							box-shadow: 0px 2px 6px rgba(0,0,0, 0.15);
-							margin-bottom: 0.5em;
-						}
+		        .channels {
+		          display: grid;
+		          grid-gap: 15px;
+		          padding: 15px;
+		          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+		        }
+		        a.channel {
+		          display: block;
+		          margin-bottom: 0.5em;
+		          color: #333;
+		          text-decoration: none;
+		        }
+		        .channel img {
+		          border-radius: 3px;
+		          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+		          width: 100%;
+		        }
+		        h1 {
+		          font-weight: 600;
+		          padding: 15px;
+		        }
+		        h2 {
+		          padding: 5px;
+		          font-size: 0.9em;
+		          font-weight: 600;
+		          margin: 0;
+		          text-align: center;
+		        }
 
-						.channel img {
-							width: 100%;
-						}
+		        .podcast {
+		          display: block;
+		          text-decoration: none;
+		          color: #333;
+		          padding: 15px;
+		          border-bottom: 1px solid rgba(0,0,0,0.2);
+		          cursor: pointer;
+		        }
+		        .podcast:hover {
+		          color: #000;
+		        }
+		        .podcast h3 {
+		          margin: 0;
+		        }
+		        .podcast .meta {
+		          color: #666;
+		          margin-top: 0.5em;
+		          font-size: 0.8em;
+		        }
+		      `}</style>
 
-						h1 {
-							font-family: Lato;
-							font-weight: 600;
-							padding: 15px;
-						}
-
-						h2 {
-							padding: 5px;
-							font-size: 0.9em;
-							font-weight: 600;
-							margin: 0;
-							text-align: center;
-						}
-					`}</style>
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: Lato;
+          background: white;
+        }
+      `}</style>
 				</div>
 			)
 	}
