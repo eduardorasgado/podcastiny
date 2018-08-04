0ddca143f53adc34281cd9bfae30167fac99689d
lal <eduardo.rasgado@hotmail.com>
2018-08-03 21:20:15 -0500

    _error.js customized with UX

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Link from 'next/link'
 // esta pagina debe de llamarse _error.js para ser reconocida como la
 // pagina de error por defecto
 export default class Error extends React.Component {
@@ -9,13 +10,42 @@ export default class Error extends React.Component {
   }
 
   render() {
+    const { statusCode } = this.props
     return (
-      <Layout>
-        <p>
-          {this.props.statusCode
-            ? `An error ${this.props.statusCode} occurred on server`
-            : 'An error occurred on client'}
-        </p>
+      <Layout title="Oh no! :(">
+        { statusCode === 404 ? 
+            <div className="message">
+              <h1>Ops! Esta página no existe</h1>
+              <p>Aqui no hay ni polvo. Que tal si regresas a la página principal
+                  a buscar algo de tu interés ;)</p>
+              <Link href={`/`} >
+                <a className="enlace">
+                  <button>Muéstrame esos Podcastinys!</button>
+                </a>
+              </Link>
+            </div>
+            :
+            <div className="message">
+              <h1> Algo salió mal :S</h1>
+              <p>Intenta nuevamente en unos segundos.</p>
+            </div>
+         }
+        <style jsx>{`
+          .message {
+            margin-top: 20vh;
+          }
+          p {
+            color: black;
+            width: 90vw;
+            margin: auto;
+          }
+          a.enlace {
+            text-decoration: none;
+          }
+          button {
+            margin-top: 50px;
+          }
+        `}</style>
       </Layout>
     )
   }
