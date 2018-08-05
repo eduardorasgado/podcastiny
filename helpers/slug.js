1947a4f01cf877ac9c29769353f184f8a3d7f85f
lal <eduardo.rasgado@hotmail.com>
2018-08-04 22:15:48 -0500

    wrote the helper

--- helpers/slug.js
+++ helpers/slug.js
@@ -1,3 +1,11 @@
 // libreria obtenida con 
 // npm add slugify
+import slugify from 'slugify'
 
+export default function slug(name) {
+	// el regex es: todo lo que no sea una palabra o un
+	// guion se rellena con '' nada
+	return slugify(name, {
+		lower: true
+	}).replace(/[^\w\-]+/g,'')
+}
